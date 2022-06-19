96129339bdad795ea211c2d5d8897de2cf62cf59
Stephen Womack <swomack111@gmail.com>
2022-06-19 13:30:22 -0500

    user entry item component

---
+++ src/components/UserEntryItem/UserEntryItem.jsx
@@ -0,0 +1,20 @@
+
+
+function UserEntryItem ({entry}) {
+
+    return(
+        <tr>
+            <td>{entry.name}</td>
+            <td>{entry.feeling}</td>
+            <td>{entry.understanding}</td>
+            <td>{entry.support}</td>
+            <td>{entry.comments}</td>
+            <td><button>Flag</button></td>
+            <td><button>Delete</button></td>
+
+            
+        </tr>
+    )
+}
+
+export default UserEntryItem
