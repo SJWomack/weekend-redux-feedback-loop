209a38f54c957a9f2e7ab15059fbde4af87de3aa
Stephen Womack <swomack111@gmail.com>
2022-06-19 11:25:17 -0500

    feeling and understanding rating components

--- src/components/FeelingRating/FeelingRating.jsx
+++ src/components/FeelingRating/FeelingRating.jsx
@@ -1,6 +1,43 @@
-function FeelingRating () {
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom';
+
+function FeelingRating() {
+
+    const [feelingLevel, setFeelingLevel] = useState('');
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+
+        dispatch({
+            type: 'ADD_FEELING_RATING',
+            payload: feelingLevel
+        })
+
+        setFeelingLevel('');
+
+        history.push('/understanding');
+    }
     return (
-        <h1>Hey</h1>
+        <>
+            <h1>How are you feeling today?</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Let us know!
+                    <input type="number"
+                        required
+                        placeholder="Required"
+                        value={feelingLevel}
+                        onChange={evt => setFeelingLevel(evt.target.value)}
+                        max={5}
+                        min={1}
+                    />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        </>
     )
 }
 
---
+++ src/components/UnderstandingRating/UnderstandingRating.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom';
+
+function UnderstandingRating() {
+
+    const [understandingLevel, setUnderstandingLevel] = useState('');
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+
+        dispatch({
+            type: 'ADD_UNDERSTANDING_RATING',
+            payload: understandingLevel
+        })
+
+        setUnderstandingLevel('');
+
+        history.push('/support');
+    }
+    return (
+        <>
+            <h1>How well are you understanding the content?</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Let us know!
+                    <input type="number"
+                        required
+                        placeholder="Required"
+                        value={understandingLevel}
+                        onChange={evt => setUnderstandingLevel(evt.target.value)}
+                        max={5}
+                        min={1}
+                    />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        </>
+    )
+}
+
+export default UnderstandingRating;
