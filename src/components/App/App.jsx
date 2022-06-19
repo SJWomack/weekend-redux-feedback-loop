eca28d8e0d30d5c39dcf37e3aee220cc0d0b6ba3
Stephen Womack <swomack111@gmail.com>
2022-06-18 22:41:41 -0500

    reducer setup/file structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
+import CommentSection from '../CommentSection/CommentSection'
+import FeelingRating from '../FeelingRating/FeelingRating'
+import SupportRating from '../SupportRating/SupportRating'
+import UnderstandingRating from '../UnderstandingRating/UnderstandingRating'
+import UserEntry from '../UserEntry/UserEntry';
 function App() {
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+      </div>
+
+      <Route path='/' exact>
+        <UserEntry />
+      </Route>
+
+
+    </Router>
+
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,62 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const feelingRating = (state = 0 , action) => {
+    switch (action.type){
+        case 'ADD_FEELING_RATING':
+            return action.payload;
+    }
+    return state;
+}
+
+const understandingRating = (state = 0, action) => {
+    switch (action.type) {
+        case 'ADD_UNDERSTANDING_RATING':
+            return action.payload;
+    }
+    return state;
+}
+
+const supportRating = (state = 0, action) => {
+    switch (action.type) {
+        case 'ADD_SUPPORT_RATING':
+            return action.payload;
+    }
+    return state;
+}
+
+const commentSection = (state = '', action) => {
+    switch (action.type) {
+        case 'ADD_COMMENT':
+            return action.payload;
+    }
+    return state;
+}
+
+const userName = (state = '', action) => {
+    switch (action.type) {
+        case 'SET_USER_NAME':
+            return action.payload;
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+       feelingRating,
+       understandingRating,
+       supportRating,
+        commentSection,
+        userName
+
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
